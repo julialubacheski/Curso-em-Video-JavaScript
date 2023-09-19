d3c7de15ac63f4937d512c047117e9ee1e550344
Julia Lubacheski <juliad.luba@gmail.com>
2023-09-19 08:37:43 -0300

    aula 10

--- Aula 11/ex009.js
+++ Aula 11/ex009.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 var país = 'Brasil'
 console.log (`Vivendo em ${país} `)
 if(país == 'Brasil') {
     console.log ('Brasileiro!')
 } else {
-=======
-var país = 'EUA'
-console.log (`Vivendo em ${país} `)
-if(país == 'Brasil') {
-    console.log ('Brasileiro')
-} esle {
->>>>>>> 320ff57959f824ee6b8855cac1e0165b7579a56e
     console.log ('Estrangeiro!')
 }
